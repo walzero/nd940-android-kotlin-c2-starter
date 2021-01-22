30ebc64f89c4a0501c812be951d6cf1d9e9f9b43
Walter Rezende <walter-zero@hotmail.com>
2021-01-22 01:20:26 -0300

    #Refactor, Update -Refactored folders -Added libraries -Created application -Planted timber -Added application to manifest

--- app/src/main/java/com/udacity/asteroidradar/data/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/Constants.kt
@@ -1,4 +1,4 @@
-package com.udacity.asteroidradar
+package com.udacity.asteroidradar.data
 
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
--- app/src/main/java/com/udacity/asteroidradar/data/PictureOfDay.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/PictureOfDay.kt
@@ -1,4 +1,4 @@
-package com.udacity.asteroidradar
+package com.udacity.asteroidradar.data
 
 import com.squareup.moshi.Json
 
